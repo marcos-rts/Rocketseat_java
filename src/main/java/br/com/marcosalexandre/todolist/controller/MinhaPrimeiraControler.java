9dc36340eafd4aecf20c48605b4e4d0b4ddf73c1
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2023-10-09 22:54:08 -0300

    configurado primeira controller

--- src/main/java/br/com/marcosalexandre/todolist/controller/MinhaPrimeiraControler.java
+++ src/main/java/br/com/marcosalexandre/todolist/controller/MinhaPrimeiraControler.java
@@ -1,5 +1,27 @@
 package br.com.marcosalexandre.todolist.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+//http://localhost:8080/ -------
 public class MinhaPrimeiraControler {
-    
+
+    /**
+     * Métodos de acesso do HTTP
+     * GET - Buscar uma informação
+     * POST - adicionar um dado/informação
+     * PUT - alterar um dado/info
+     * DELETE - remover um dado 
+     * PATCH - alterar somente uma parte da info/dado
+     * 
+     */
+
+     // Método (funcionalidade) de uma classe
+     @GetMapping("/primeiroMetodo")
+    public String primeiraMensagem(){
+        return "Funcionou";
+    }
 }
