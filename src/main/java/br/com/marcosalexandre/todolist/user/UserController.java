00c6c868db54d7b18e9a014ad8d87653fe0754d0
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2023-10-09 23:07:37 -0300

    finalização usercontroler em modo public

--- src/main/java/br/com/marcosalexandre/todolist/user/UserController.java
+++ src/main/java/br/com/marcosalexandre/todolist/user/UserController.java
@@ -1,11 +1,34 @@
 package br.com.marcosalexandre.todolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * Modificador
+ * Modificador:
  * public
  * private
  * protected
  */
+@RestController
+@RequestMapping("/users")
 public class UserController {
     
+    /**
+     * String (texto)
+     * Integer (int) numeros inteiros
+     * Double (double) numeros 0.0000
+     * Float (float) Numeros 0.000
+     * char (A C)
+     * Date (data)
+     * void
+     */
+    /*
+     * Body
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.name);
+    }
 }
