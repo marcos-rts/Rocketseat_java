c36485127b6648cbe6b9e4598d57cfae89907c78
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2023-10-12 03:08:39 -0300

    configurando interface

--- src/main/java/br/com/marcosalexandre/todolist/user/UserController.java
+++ src/main/java/br/com/marcosalexandre/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.marcosalexandre.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
     
     /**
      * String (texto)
@@ -28,7 +32,9 @@ public class UserController {
      * Body
      */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel){
+        // System.out.println(userModel.getName());
+        var userCreated  = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
