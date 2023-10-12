5accc2c2ba40f2b464acbbce9e51366b2773ff1c
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2023-10-12 03:21:44 -0300

    configurando return de mensagens e codigos

--- src/main/java/br/com/marcosalexandre/todolist/user/UserController.java
+++ src/main/java/br/com/marcosalexandre/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.marcosalexandre.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,9 +34,18 @@ public class UserController {
      * Body
      */
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
         // System.out.println(userModel.getName());
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            // System.out.println("Usuario ja existe");
+            // mensagem de erro
+            // status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
+        }
+
         var userCreated  = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
